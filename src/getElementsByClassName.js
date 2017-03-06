f98ba874d2c9b11bd65804a769e455866bcd72c3
Ivana <ivana.he.lili@gmail.com>
2017-03-05 19:44:40 -0800

    "make getElementsByClassName work"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
+// var result=[];
+// var element = document.documentElement;
+// var children = $(element).children();
+
 var getElementsByClassName = function(className
 ) {
-  // your code here
+//no more siblings and no more children 
+
+    // if ($(element).hasClass(className)){
+    //     result.push(element);
+    // }
+    var results = [];
+    return getElementsByClassNameHelper(className, document.documentElement, results);
 };
+
+function getElementsByClassNameHelper(className, element, results){
+    if ($(element).hasClass(className)){
+        results.push(element);
+    }
+    for (var i=0; i<$(element).children().length; i++){
+        getElementsByClassNameHelper(className, $(element).children()[i], results);
+    }
+
+    return results;
+}
+
+// getElementsByClassName("targetClassName")("targetClassName", element)
+
